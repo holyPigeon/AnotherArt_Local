bc718918dbb343b52bdbb6346285a7476ab95769
jae woo Jung <awfjol2008@naver.com>
2022-08-06 00:57:26 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="container">
+    <div>
+      <button type="button" @click="page[0] = 1; page[1] = 0;" class="btn btn-outline-dark">1</button>
+      <button type="button" @click="page[0] = 0; page[1] = 1;" class="btn btn-outline-dark">2</button>
+    </div>
+    <TermsOfService v-if="page[0] = 1"></TermsOfService>
     <RegisterModals></RegisterModals>
-    <TermsOfService></TermsOfService>
-    <UserRegister></UserRegister>
+    <UserRegister v-if="page[1] = 1"></UserRegister>
   </div>
 
 </template>
@@ -16,56 +20,16 @@ export default {
   name: 'App',
   data() {
     return {
-
+      page: [1, 0],
     }
   },
   methods: {
-    execDaumPostcode() {
-      new window.daum.Postcode({
-        oncomplete: (data) => {
-          if (this.extraAddress !== "") {
-            this.extraAddress = "";
-          }
-          if (data.userSelectedType === "R") {
-            // 사용자가 도로명 주소를 선택했을 경우
-            this.address = data.roadAddress;
-          } else {
-            // 사용자가 지번 주소를 선택했을 경우(J)
-            this.address = data.jibunAddress;
-          }
-
-          // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
-          if (data.userSelectedType === "R") {
-            // 법정동명이 있을 경우 추가한다. (법정리는 제외)
-            // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
-            if (data.bname !== "" && /[동|로|가]$/g.test(data.bname)) {
-              this.extraAddress += data.bname;
-            }
-            // 건물명이 있고, 공동주택일 경우 추가한다.
-            if (data.buildingName !== "" && data.apartment === "Y") {
-              this.extraAddress +=
-                this.extraAddress !== ""
-                  ? `, ${data.buildingName}`
-                  : data.buildingName;
-            }
-            // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
-            if (this.extraAddress !== "") {
-              this.extraAddress = `(${this.extraAddress})`;
-            }
-          } else {
-            this.extraAddress = "";
-          }
-          // 우편번호를 입력한다.
-          this.postcode = data.zonecode;
-        },
-      }).open();
-    },
   },
   components: {
     TermsOfService,
     UserRegister,
     RegisterModals,
-  }
+}
 }
 </script>
 
